c393587731c24834fd89681f25093107f7999619
tanzeelashiq <124078387+tanzeelashiq@users.noreply.github.com>
2025-12-29 11:12:03 +0530

    Enable trading by setting ENABLE_TRADING to True

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 TRADE_AMOUNT_INR = 200        # Fixed trade amount
 ALLOWED_SYMBOLS = ["BTCINR"]  # Start with BTC only
-ENABLE_TRADING = False         # Emergency kill switch
+ENABLE_TRADING = True         # Emergency kill switch
